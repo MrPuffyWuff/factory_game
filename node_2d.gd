18a5c364d10b88ef81e52fc947744d14f4ac5551
Justbetter14 <145606090+Justbetter14@users.noreply.github.com>
2024-11-25 19:03:58 -0500

    Added Cell.tscn
    Added the base for all tiles and machines

---
+++ node_2d.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var gridsize := [2,3]
+var xPos := 0
+var yPos := 0
+var xFinalPos = xPos + (16 * gridsize[0])
+var yFinalPos = yPos + (16 * gridsize[1])
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass

3146db642d555b2bfdad61cb45b853a675a03709
MrPuffyWuff <jasper13.com@gmail.com>
2024-11-29 14:51:03 -0500

    Input Mapping
    Added Input Mapping
     - User can now use WASD and arrow keys to move the player

--- scenes/player.gd
+++ scenes/player.gd
@@ -7,15 +7,15 @@ func _ready() -> void:
 	pass
 
 func _physics_process(_delta):
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("movement_right"):
 		velocity.x = speed
-	elif Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("movement_left"):
 		velocity.x = -1 * speed
 	else:
 		velocity.x = 0
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("movement_up"):
 		velocity.y = -1 * speed
-	elif Input.is_action_pressed("ui_down"):
+	elif Input.is_action_pressed("movement_down"):
 		velocity.y = speed
 	else:
 		velocity.y = 0
